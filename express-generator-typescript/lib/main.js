7a2c1ab0a82e512199f7907acde4edf87d893f2b
seanpmaxwell <seanpmaxwell1@gmail.com>
2019-05-31 23:35:30 -0700

    started adding project files

---
+++ express-generator-typescript/lib/main.js
@@ -0,0 +1,7 @@
+/**
+ * Create new express-generator-typescript project.
+ *
+ * created by Sean Maxwell, 5/31/2019
+ */
+console.log('horse');
+//# sourceMappingURL=main.js.map
---
+++ express-generator-typescript/lib/project-files/src/controllers/ParentController.ts
@@ -0,0 +1,1 @@
+// pick up here
