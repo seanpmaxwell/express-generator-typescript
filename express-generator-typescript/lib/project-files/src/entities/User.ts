3c1382dcbfef9b18e0b8cfa96647e1f321f5e0ab
seanpmaxwell <seanpmaxwell1@gmail.com>
2019-06-01 16:18:42 -0700

    created simple api that updates html

--- express-generator-typescript/lib/project-files/src/entities/User.ts
+++ express-generator-typescript/lib/project-files/src/entities/User.ts
@@ -1,5 +1,12 @@
-export class User {
+class User {
 
     public name: string;
     public email: string;
+
+    constructor(name: string, email: string) {
+        this.name = name;
+        this.email = email;
+    }
 }
+
+export default User;
