25f1bcff62c405fa73a606688a802000a53887e6
seanpmaxwell <seanpmaxwell1@gmail.com>
2019-06-01 15:20:22 -0700

    downloaded need packages

--- express-generator-typescript/lib/project-files/src/controllers/ParentController.ts
+++ express-generator-typescript/lib/project-files/src/controllers/ParentController.ts
@@ -1,1 +1,9 @@
-// pick up here
+import { Children, Controller } from '@overnightjs/core';
+import { UserController } from './user/UserController';
+
+
+@Controller('api')
+@Children([
+    new UserController(),
+])
+export class ParentController { }
