ea36961186e031d2d86190c8e3bc04b58a9973bf
seanpmaxwell <seanpmaxwell1@gmail.com>
2019-06-01 17:00:56 -0700

    added tests

--- express-generator-typescript/lib/project-files/src/controllers/user/UserController.ts
+++ express-generator-typescript/lib/project-files/src/controllers/user/UserController.ts
@@ -1,17 +1,33 @@
 import { Controller, Get } from '@overnightjs/core';
 import { Request, Response } from 'express-serve-static-core';
-import { OK } from 'http-status-codes';
-import User from '../../entities/User'
+import {BAD_REQUEST, OK} from 'http-status-codes';
+import { User } from '../../entities/User'
 
 
 @Controller('users')
 export class UserController {
 
+    public readonly CURRENT_ROUTE = '/users';
+    public readonly REQ_PARAM_MISSING = 'One of the required params is missing';
+
 
     @Get(':name/:email')
     private get(req: Request, res: Response) {
-        return res.status(OK).json({
-            user: new User(req.params.name, req.params.email),
-        })
+        try {
+            const { name, email } = req.params;
+            if (name && email && email !== 'undefined') {
+                return res.status(OK).json({
+                    user: new User(name, email),
+                });
+            } else {
+                return res.status(BAD_REQUEST).json({
+                    error: this.REQ_PARAM_MISSING,
+                });
+            }
+        } catch (err) {
+            return res.status(BAD_REQUEST).json({
+                error: err.message,
+            });
+        }
     }
 }
