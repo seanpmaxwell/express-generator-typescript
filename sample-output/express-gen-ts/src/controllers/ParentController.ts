4e59d043889b535145aa9cfbe1c3aa07ec710470
seanpmaxwell <seanpmaxwell1@gmail.com>
2019-06-02 22:26:32 -0700

    fixed some linting issues

---
+++ sample-output/express-gen-ts/src/controllers/ParentController.ts
@@ -0,0 +1,8 @@
+import { Children, Controller } from '@overnightjs/core';
+import { UserController } from './user/UserController';
+
+@Controller('api')
+@Children([
+    new UserController(),
+])
+export class ParentController { }
