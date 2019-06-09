1bfb702b299ee90a88e297700f956200b87fccc6
seanpmaxwell <seanpmaxwell1@gmail.com>
2019-06-09 16:55:34 -0700

    updated the readme

--- sample-output/express-gen-ts/dist/routes/Base.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const Users_1 = require("./users/Users");
-const router = express_1.Router();
-const path = '/api';
-router.use(Users_1.default.path, Users_1.default.router);
-exports.default = { router, path };
