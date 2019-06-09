000e19de018b54010f65341d5f1efc6ca60faf63
seanpmaxwell <seanpmaxwell1@gmail.com>
2019-06-09 16:55:34 -0700

    updated the readme

--- sample-output/express-gen-ts/dist/Start.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-require("../env/loadEnv");
-const Logger_1 = require("./Logger");
-const Server_1 = require("./Server");
-const port = Number(process.env.PORT || 3000);
-Server_1.default.listen(port, () => {
-    Logger_1.default.info('Express server started on port: ' + port);
-});
