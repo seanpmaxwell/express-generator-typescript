480bcfba87ad71393f75b3093907a18c3429236a
seanpmaxwell <seanpmaxwell1@gmail.com>
2019-06-15 20:44:04 -0700

    adding tsconfig paths

---
+++ sample-output/express-gen-ts/dist/Server.js
@@ -0,0 +1,23 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const tslib_1 = require("tslib");
+const path_1 = tslib_1.__importDefault(require("path"));
+const cookie_parser_1 = tslib_1.__importDefault(require("cookie-parser"));
+const morgan_1 = tslib_1.__importDefault(require("morgan"));
+const express_1 = tslib_1.__importDefault(require("express"));
+const Base_1 = tslib_1.__importDefault(require("./routes/Base"));
+const app = express_1.default();
+app.use(morgan_1.default('dev'));
+app.use(express_1.default.json());
+app.use(express_1.default.urlencoded({ extended: false }));
+app.use(cookie_parser_1.default());
+app.use(express_1.default.static(path_1.default.join(__dirname, 'public')));
+app.use(Base_1.default.path, Base_1.default.router);
+const viewsDir = path_1.default.join(__dirname, 'views');
+app.set('views', viewsDir);
+const staticDir = path_1.default.join(__dirname, 'public');
+app.use(express_1.default.static(staticDir));
+app.get('*', (req, res) => {
+    res.sendFile('index.html', { root: viewsDir });
+});
+exports.default = app;
