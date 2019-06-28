b4c34feb8f7f73430dac8baa6f2c5c769844aeeb
seanpmaxwell <seanpmaxwell1@gmail.com>
2019-06-27 22:38:17 -0700

    adding mock db cont

--- express-generator-typescript/lib/project-files/src/routes/users/Users.test.ts
+++ express-generator-typescript/lib/project-files/src/routes/users/Users.test.ts
@@ -20,37 +20,37 @@ describe('Users Routes', () => {
         done();
     });
 
-    describe('GET "/api/users/:name/:email"', () => {
+    describe('GET "/api/users"', () => {
 
-        const user = new User('sean', 'sean@express-generator-typescript.com');
-
-        it(`should return a JSON object with a user object and a status code of ${OK} if the
-            request was successful`, (done) => {
-
-            agent.get(`${usersPath}/${user.name}/${user.email}`)
-                .end((err: Error, res: Response) => {
-                    if (err) { logger.error(err); }
-                    expect(res.status).toBe(OK);
-                    const retUser = new User(res.body.user);
-                    expect(retUser).toEqual(user);
-                    done();
-                });
-        });
-
-        const {
-            getUserParamMissing
-        } = userRouterItems;
-
-        it(`should return a JSON object with the error "${getUserParamMissing}" and a status code
-            of "${BAD_REQUEST}" if one of the required user params is missing`, (done) => {
-
-            agent.get(`${usersPath}/${user.name}/undefined`)
-                .end((err: Error, res: Response) => {
-                    if (err) { logger.error(err); }
-                    expect(res.status).toBe(BAD_REQUEST);
-                    expect(res.body.error).toBe(getUserParamMissing);
-                    done();
-                });
-        });
+        // const user = new User('sean', 'sean@express-generator-typescript.com');
+        //
+        // it(`should return a JSON object with a user object and a status code of ${OK} if the
+        //     request was successful`, (done) => {
+        //
+        //     agent.get(`${usersPath}/${user.name}/${user.email}`)
+        //         .end((err: Error, res: Response) => {
+        //             if (err) { logger.error(err); }
+        //             expect(res.status).toBe(OK);
+        //             const retUser = new User(res.body.user);
+        //             expect(retUser).toEqual(user);
+        //             done();
+        //         });
+        // });
+        //
+        // const {
+        //     getUserParamMissing
+        // } = userRouterItems;
+        //
+        // it(`should return a JSON object with the error "${getUserParamMissing}" and a status code
+        //     of "${BAD_REQUEST}" if one of the required user params is missing`, (done) => {
+        //
+        //     agent.get(`${usersPath}/${user.name}/undefined`)
+        //         .end((err: Error, res: Response) => {
+        //             if (err) { logger.error(err); }
+        //             expect(res.status).toBe(BAD_REQUEST);
+        //             expect(res.body.error).toBe(getUserParamMissing);
+        //             done();
+        //         });
+        // });
     });
 });
