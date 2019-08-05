ace3cb05264da13e671401af88c773735edd1585
seanpmaxwell <seanpmaxwell1@gmail.com>
2019-08-05 14:22:44 -0700

    updated nodemon.test file

--- sample-output/express-gen-ts/dist/daos/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const usingMockDb = (process.env.USE_MOCK_DB || '').toLowerCase();
-let userDaoPath = './User/UserDao';
-if (usingMockDb === 'true') {
-    userDaoPath += '.mock';
-}
-exports.UserDao = require(userDaoPath).UserDao;
