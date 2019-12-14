96e9a09875d13c20f9c697bc50db92a1af516164
seanpmaxwell <seanpmaxwell1@gmail.com>
2019-12-14 10:57:54 -0800

    removing duplicate status things

--- express-generator-typescript/lib/project-files/src/Server.ts
+++ express-generator-typescript/lib/project-files/src/Server.ts
@@ -13,7 +13,6 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 app.use('/api', BaseRouter);
 
 /**
