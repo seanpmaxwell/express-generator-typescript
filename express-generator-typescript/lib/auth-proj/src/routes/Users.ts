a8fe20b51c740beb0ab6339e55a9b9ad2f753493
Sean Maxwell <smaxwell@athenahealth.com>
2020-03-09 15:54:07 -0700

    added express-async-errors

--- express-generator-typescript/lib/auth-proj/src/routes/Users.ts
+++ express-generator-typescript/lib/auth-proj/src/routes/Users.ts
@@ -3,7 +3,6 @@ import { BAD_REQUEST, CREATED, OK } from 'http-status-codes';
 import { ParamsDictionary } from 'express-serve-static-core';
 
 import UserDao from '@daos/User/UserDao.mock';
-import logger from '@shared/Logger';
 import { paramMissingError } from '@shared/constants';
 import { adminMW } from './middleware';
 import { UserRoles } from '@entities/User';
@@ -19,15 +18,8 @@ const userDao = new UserDao();
  ******************************************************************************/
 
 router.get('/all', async (req: Request, res: Response) => {
-    try {
-        const users = await userDao.getAll();
-        return res.status(OK).json({users});
-    } catch (err) {
-        logger.error(err.message, err);
-        return res.status(BAD_REQUEST).json({
-            error: err.message,
-        });
-    }
+    const users = await userDao.getAll();
+    return res.status(OK).json({users});
 });
 
 
@@ -36,24 +28,17 @@ router.get('/all', async (req: Request, res: Response) => {
  ******************************************************************************/
 
 router.post('/add', async (req: Request, res: Response) => {
-    try {
-        // Check parameters
-        const { user } = req.body;
-        if (!user) {
-            return res.status(BAD_REQUEST).json({
-                error: paramMissingError,
-            });
-        }
-        // Add new user
-        user.role = UserRoles.Standard;
-        await userDao.add(user);
-        return res.status(CREATED).end();
-    } catch (err) {
-        logger.error(err.message, err);
+    // Check parameters
+    const { user } = req.body;
+    if (!user) {
         return res.status(BAD_REQUEST).json({
-            error: err.message,
+            error: paramMissingError,
         });
     }
+    // Add new user
+    user.role = UserRoles.Standard;
+    await userDao.add(user);
+    return res.status(CREATED).end();
 });
 
 
@@ -62,24 +47,17 @@ router.post('/add', async (req: Request, res: Response) => {
  ******************************************************************************/
 
 router.put('/update', async (req: Request, res: Response) => {
-    try {
-        // Check Parameters
-        const { user } = req.body;
-        if (!user) {
-            return res.status(BAD_REQUEST).json({
-                error: paramMissingError,
-            });
-        }
-        // Update user
-        user.id = Number(user.id);
-        await userDao.update(user);
-        return res.status(OK).end();
-    } catch (err) {
-        logger.error(err.message, err);
+    // Check Parameters
+    const { user } = req.body;
+    if (!user) {
         return res.status(BAD_REQUEST).json({
-            error: err.message,
+            error: paramMissingError,
         });
     }
+    // Update user
+    user.id = Number(user.id);
+    await userDao.update(user);
+    return res.status(OK).end();
 });
 
 
@@ -88,16 +66,9 @@ router.put('/update', async (req: Request, res: Response) => {
  ******************************************************************************/
 
 router.delete('/delete/:id', async (req: Request, res: Response) => {
-    try {
-        const { id } = req.params as ParamsDictionary;
-        await userDao.delete(Number(id));
-        return res.status(OK).end();
-    } catch (err) {
-        logger.error(err.message, err);
-        return res.status(BAD_REQUEST).json({
-            error: err.message,
-        });
-    }
+    const { id } = req.params as ParamsDictionary;
+    await userDao.delete(Number(id));
+    return res.status(OK).end();
 });
 
 
