5cb8c81981ddaf700d3d8cc6f96415a591f88eb4
Nuttapat Koonarangsri <webmaster@hackinteach.com>
2020-06-04 23:14:13 +0700

    throw error if tsc fails to build

--- express-generator-typescript/lib/project-files/build.js
+++ express-generator-typescript/lib/project-files/build.js
@@ -9,7 +9,12 @@ try {
     fs.copySync('./src/public', './dist/public');
     fs.copySync('./src/views', './dist/views');
     // Transpile the typescript files
-    childProcess.exec('tsc --build tsconfig.prod.json');
+    const proc = childProcess.exec('tsc --build tsconfig.prod.json');
+    proc.on('close', (code) => {
+        if (code !== 0) {
+            throw Error("Build failed")
+        }
+    })
 } catch (err) {
     console.log(err);
 }
