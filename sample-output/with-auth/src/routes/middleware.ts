5893f44d91197a56426e5c2b270a9525bd75492c
seanpmaxwell <spmaxwell7@gmail.com>
2022-02-04 14:41:07 -0800

    update to structured procedural cont

--- sample-output/with-auth/src/routes/middleware.ts
+++ sample-output/with-auth/src/routes/middleware.ts
@@ -1,31 +1,38 @@
 import StatusCodes from 'http-status-codes';
 import { Request, Response, NextFunction } from 'express';
 
-import { UserRoles } from '@entities/User';
-import { cookieProps } from '@shared/constants';
-import { JwtService } from '@shared/JwtService';
+import { UserRoles } from '@models/user';
+import { cookieProps } from '@routes/auth';
+import jwtUtil from '@util/jwtUtil';
 
 
-
-const jwtService = new JwtService();
+// Constants
 const { UNAUTHORIZED } = StatusCodes;
+const jwtNotPresentErr = 'JWT not present in signed cookie.';
 
 
-// Middleware to verify if user is an admin
-export const adminMW = async (req: Request, res: Response, next: NextFunction) => {
+/**
+ * Middleware to verify if user is an admin.
+ * 
+ * @param req 
+ * @param res 
+ * @param next 
+ * @returns 
+ */
+export async function adminMw(req: Request, res: Response, next: NextFunction) {
     try {
         // Get json-web-token
         const jwt = req.signedCookies[cookieProps.key];
         if (!jwt) {
-            throw Error('JWT not present in signed cookie.');
+            throw Error(jwtNotPresentErr);
         }
         // Make sure user role is an admin
-        const clientData = await jwtService.decodeJwt(jwt);
-        if (clientData.role === UserRoles.Admin) {
-            res.sessionUser = clientData;
+        const clientData = await jwtUtil.decode(jwt);
+        if (typeof clientData === 'object' && clientData.role === UserRoles.Admin) {
+            res.locals.sessionUser = clientData;
             next();
         } else {
-            throw Error('JWT not present in signed cookie.');
+            throw Error(jwtNotPresentErr);
         }
     } catch (err) {
         return res.status(UNAUTHORIZED).json({
